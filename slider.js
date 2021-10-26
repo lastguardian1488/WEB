67aa1bcceb93c0d4e20d3b4a202cc5de452679b3
lastguardian1488 <taras1246@gmail.com>
2021-10-26 15:26:29 +0300

    Added slick slider using jquery

---
+++ slider.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('.slick-slider').slick({
+    infinite: true,
+    dots: true,
+    slidesToScroll: 4,
+    slidesToShow: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
+});
